ff35e97c6e900ac6a128505c527bcf4db303bdca
ThanNong <nguyenhoangtai2k@gmail.com>
2022-08-18 14:29:13 +0700

    add package

---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from '../components/Ui/Header';
+import Sidebar from '../components/Ui/Sidebar';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Sidebar/>
+    );
+  }
+}
